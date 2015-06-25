996dbb914d58827ff86e7766da5f7ed460625cec
Michael Williams <dinosaur@riseup.net>
2015-06-26 01:54:30 +1200

    use raf

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var inherits = require('inherits')
 var Writable = require('readable-stream').Writable
 var React = require('react')
+var raf = require('raf')
 
 module.exports = ReactRenderStream
 
@@ -22,9 +23,14 @@ function ReactRenderStream (opts) {
 ReactRenderStream.prototype._write = write
 
 function write (props, enc, cb) {
-  React.render(
-    this.Factory(props),
-    this.element,
-    cb
-  )
+  var Factory = this.Factory
+  var element = this.element
+
+  raf(function () {
+    React.render(
+      Factory(props),
+      element,
+      cb
+    )
+  })
 }
